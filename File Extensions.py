4cd9afcdab336a98b2dc8d00d623fa013a7377d5
Tarsow <98370872+Tarsow@users.noreply.github.com>
2023-12-19 19:32:06 -0300

    Create File Extensions.py

---
+++ File Extensions.py
@@ -0,0 +1,24 @@
+def main():
+    x = input("File Name: ").lower().strip()
+    x1, x2 = x.split(".")
+
+    match x2:
+        case "gif":
+            print("image/gif")
+        case "jpg":
+            print("image/jpeg")
+        case "jpeg":
+            print("image/jpeg")
+        case "png":
+            print("image/png")
+        case "pdf":
+            print("application/pdf")
+        case "txt":
+            print("text/plain")
+        case "zip":
+            print("application/zip")
+        case _:
+            print("application/octet-stream")
+
+
+main()
